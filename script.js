fee85b64d73996e1bbe7264f888a744d2f4b9482
Vihan <vihan.vnt@gmail.com>
2025-06-15 15:28:34 +0530

    hk

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("click",function(e){
+    const length = document.getElementById("password-length").value
+    const val = document.getElementById("include-numbers").checked
+    console.log(length)
+    console.log(val)
+    const upperindex = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowerindex = "abcdefghijklmnopqrstuvwxyz"
+    const numbers = '0123456789'
+    let index
+    //checked case = jahan with numbers
+    if(val === true){
+        index = upperindex + lowerindex + numbers        
+    }
+    //default case= only alphabets
+    else{
+        index = upperindex + lowerindex
+    }
+    let password = '';
+    for(let i = 0; i < length; i++){
+        const randomid = Math.floor(Math.random() * index.length);
+        password += index[randomid]
+        console.log(password)
+    }
+    const passwordfield = document.getElementById("password-column")
+    passwordfield.value = password
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
